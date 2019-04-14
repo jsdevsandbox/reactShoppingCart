1f97e3efe07dd7dfb91c67c5a96d8c8b2cbe9af9
jsdevsandbox <jsdevsandbox@gmail.com>
2019-04-13 17:59:25 -0700

    adding unit tests to the NavBar component

---
+++ src/components/navbar.test.js
@@ -0,0 +1,45 @@
+import React from "react";
+import NavBar from "./navbar";
+import { configure, mount, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
+
+describe("unit tests for navbar component", () => {
+  let component;
+  beforeEach(() => {
+    component = shallow(<NavBar />);
+  });
+
+  it("should render one <NavBar /> component", () => {
+    expect(component).toHaveLength(1);
+  });
+
+  it("should have one <nav> element", () => {
+    expect(component.find("nav")).toHaveLength(1);
+  });
+
+  it("<a> element should have correct text", () => {
+    expect(component.find("a").text()).toBe("Navbar ");
+  });
+
+  it("nav element should have correct classes", () => {
+    expect(component.find("nav").hasClass("navbar-light")).toEqual(true);
+    expect(component.find("nav").hasClass("bg-light")).toEqual(true);
+  });
+
+  it("should have one <span> element", () => {
+    expect(component.find("span")).toHaveLength(1);
+  });
+
+  it("span element should have correct classes", () => {
+    expect(component.find("span").hasClass("badge-pill")).toEqual(true);
+    expect(component.find("span").hasClass("badge-secondary")).toEqual(true);
+  });
+
+  it("span element should have the correct text", () => {
+    component = shallow(<NavBar totalCounters={3} />);
+    component.setProps({ totalCounters: 3 });
+    expect(component.find("span").text()).toBe("3");
+    expect(component.find("a").text()).toBe("Navbar 3");
+  });
+});
